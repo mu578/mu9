d94f384dcdbc5a993979369ed001aacc18ff21e8
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-09 19:23:50 -0800

    iterator re-factoring access

--- mu9/mu9_iterator.h
+++ mu9/mu9_iterator.h
@@ -63,27 +63,42 @@ MU0_BEGIN_CDECL
 #	define mu9_const_iterator_distance(_Tp, __first, __last) mu9_const_difference((mu0_const_cast(_Tp *, __last) - mu0_const_cast(_Tp *, __first)))
 
 //#!
-//#! macro<_Tp, _Dist>(_Tp &* __it, _Dist & __n = +1) : _Tp *
+//#! macro<_Tp, _Dist>(_Tp &* __it, _Dist & __n) : void
 //#!
-#	define mu9_iterator_advance_n(_Tp, __it, __n)            (__it) += mu0_distance(__n)
-#	define mu9_iterator_advance(_Tp, __it)                   (__it) += 1
+#	define mu9_iterator_advance_n(_Tp, __it, __n) \
+mu0_scope_begin                                  \
+	(__it) += mu0_distance(__n);                  \
+mu0_scope_end
+
+//#!
+//#! macro<_Tp, _Dist>(_Tp &* __it, _Dist & __n = +1) : void
+//#!
+#	define mu9_iterator_advance(_Tp, __it)                   mu9_iterator_advance_n(_Tp, __it, +1);
+#	define mu9_iterator_forward(_Tp, __it)                   mu9_iterator_advance(_Tp, __it);
+
+//#!
+//#! macro<_Tp, _Dist>(_Tp &* __it, _Dist & __n = -1) : void
+//#!
+#	define mu9_iterator_backward(_Tp, __it)                  mu9_iterator_advance_n(_Tp, __it, -1);
 
 //#!
 //#! macro<_Tp, _Dist>(_Tp &* __it, _Dist & __n = +1) : _Tp *
 //#!
-#	define mu9_iterator_next_n(_Tp, __it, __n)               mu9_iterator_advance(_Tp, __it, ((__n) < 0 ? -(__n) :  (__n)))
-#	define mu9_iterator_next(_Tp, __it)                      mu9_iterator_advance(_Tp, __it, +1)
+#	define mu9_iterator_next_n(_Tp, __it, __n)               ((it) + mu0_distance((__n) < 0 ? -(__n) : (__n)))
+#	define mu9_iterator_next(_Tp, __it)                      ((it) + mu0_distance(+1)) 
 
 //#!
 //#! macro<_Tp, _Dist>(_Tp &* __it, _Dist & __n = -1) : _Tp *
 //#!
-#	define mu9_iterator_prev_n(_Tp, __it, __n)               mu9_iterator_advance(_Tp, __it, ((__n) < 0 ?  (__n) : -(__n)))
-#	define mu9_iterator_prev(_Tp, __it)                      mu9_iterator_advance(_Tp, __it, -1)
+#	define mu9_iterator_prev_n(_Tp, __it, __n)               ((it) + mu0_distance((__n) < 0 ? (__n) : -(__n)))
+#	define mu9_iterator_prev(_Tp, __it)                      ((it) + mu0_distance(-1))
 
 #	define mu9_distance(_Tp, __first, __last)                mu9_iterator_distance(_Tp, __first, __last)
 #	define mu9_const_distance(_Tp, __first, __last)          mu9_const_iterator_distance(_Tp, __first, __last)
+
 #	define mu9_advance_n(_Tp, __it, __n)                     mu9_iterator_advance_n(_Tp, __it, __n)
 #	define mu9_advance(_Tp, __it)                            mu9_iterator_advance(_Tp, __it)
+
 #	define mu9_next_n(_Tp, __it, __n)                        mu9_iterator_next_n(_Tp, __it, __n)
 #	define mu9_next(_Tp, __it)                               mu9_iterator_next(_Tp, __it)
 #	define mu9_prev_n(_Tp, __it, __n)                        mu9_iterator_prev_n(_Tp, __it, __n)
