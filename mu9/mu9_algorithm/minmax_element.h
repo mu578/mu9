774ee5ca122893f22770e9d24b4eb83f2caba4ca
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-12 10:42:51 -0800

    wip mu9

--- mu9/mu9_algorithm/minmax_element.h
+++ mu9/mu9_algorithm/minmax_element.h
@@ -22,51 +22,6 @@
 
 MU0_BEGIN_CDECL
 
-#	define mu9_min_element1(_Tp, __first, __last, __d_result) \
-mu0_scope_begin                                              \
-	_Tp * __mu9_min_element1_i__ = __last;                    \
-	if (__first != __last) {                                  \
-		__mu9_min_element1_i__ = __first;                      \
-		++__first;                                             \
-		for (; __first != __last; ++__first) {                 \
-			if (*__first < *__mu9_min_element1_i__) {           \
-				__mu9_min_element1_i__ = __first;                \
-			}                                                   \
-		}                                                      \
-	}                                                         \
-	__d_result = __mu9_min_element1_i__;                      \
-mu0_scope_end
-
-#	define mu9_min_element2(_Tp, __first, __last, __comp_fn, __d_result) \
-mu0_scope_begin                                                         \
-	_Tp * __mu9_min_element2_i__ = __last;                               \
-	if (__first != __last) {                                             \
-		__mu9_min_element2_i__ = __first;                                 \
-		++__first;                                                        \
-		for (; __first != __last; ++__first) {                            \
-			if (__comp_fn(*__first,  *__mu9_min_element2_i__)) {           \
-				__mu9_min_element2_i__ = __first;                           \
-			}                                                              \
-		}                                                                 \
-	}                                                                    \
-	__d_result = __mu9_min_element2_i__;                                 \
-mu0_scope_end
-
-#	define mu9_min_element3(_Tp, __first, __last, __sort_fn, __ctx, __d_result) \
-mu0_scope_begin                                                                \
-	_Tp * __mu9_min_element3_i__ = __last;                                      \
-	if (__first != __last) {                                                    \
-		__mu9_min_element3_i__ = __first;                                        \
-		++__first;                                                               \
-		for (; __first != __last; ++__first) {                                   \
-			if (__sort_fn(*__first,  *__mu9_min_element3_i__, __ctx)) {           \
-				__mu9_min_element3_i__ = __first;                                  \
-			}                                                                     \
-		}                                                                        \
-	}                                                                           \
-	__d_result = __mu9_min_element3_i__;                                        \
-mu0_scope_end
-
 MU0_END_CDECL
 
 #endif /* !MU9_MINMAX_ELEMENT_H */
