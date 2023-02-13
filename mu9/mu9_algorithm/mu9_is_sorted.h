3e96a22edf44c5b360a7d19dbe8dc258a9e1fa23
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-12 18:06:01 -0800

    is_sorted

---
+++ mu9/mu9_algorithm/mu9_is_sorted.h
@@ -0,0 +1,50 @@
+//
+// # -*- coding: utf-8, tab-width: 3 -*-
+
+//                                                          ___                                             //
+//                                                         / _ \                                            //
+//                                            _   _  _   _( (_) |                                           //
+//                                           | | | || | | |\__, |                                           //
+//                                           | |_| || |_| |  / /                                            //
+//                                           | ._,_| \___/  /_/                                             //
+//                                           | |                                                            //
+//                                           |_|                                                            //
+
+// mu9_is_sorted.h
+//
+// Copyright (C) 2023 mu578. All rights reserved.
+//
+
+#include <mu9/mu9_algorithm/mu9_is_sorted_until.h>
+
+#ifndef MU9_IS_SORTED_H
+#define MU9_IS_SORTED_H 1
+
+MU0_BEGIN_CDECL
+
+#	define mu9_is_sorted1(_Tp, __first, __last, __d_result)                 \
+mu0_scope_begin                                                            \
+	_Tp * __mu9_is_sorted1__d__;                                            \
+	mu9_is_sorted_until1(_Tp, __first, __last, __mu9_is_sorted1__d__);      \
+	__d_result == (__mu9_is_sorted1__d__ == __last) ? mu0_true : mu0_false; \
+mu0_scope_end
+
+#	define mu9_is_sorted2(_Tp, __first, __last, __comp_fn, __d_result)            \
+mu0_scope_begin                                                                  \
+	_Tp * __mu9_is_sorted2__d__;                                                  \
+	mu9_is_sorted_until2(_Tp, __first, __last, __comp_fn, __mu9_is_sorted2__d__); \
+	__d_result == (__mu9_is_sorted2__d__ == __last) ? mu0_true : mu0_false;       \
+mu0_scope_end
+
+#	define mu9_is_sorted3(_Tp, __first, __last, __sort_fn, __ctx, __d_result)            \
+mu0_scope_begin                                                                         \
+	_Tp * __mu9_is_sorted3__d__;                                                         \
+	mu9_is_sorted_until3(_Tp, __first, __last, __sort_fn, __ctx, __mu9_is_sorted3__d__); \
+	__d_result == (__mu9_is_sorted3__d__ == __last) ? mu0_true : mu0_false;              \
+mu0_scope_end
+
+MU0_END_CDECL
+
+#endif /* !MU9_IS_SORTED_H */
+
+/* EOF */
---
+++ mu9/mu9_algorithm/mu9_is_sorted_until.h
@@ -0,0 +1,77 @@
+//
+// # -*- coding: utf-8, tab-width: 3 -*-
+
+//                                                          ___                                             //
+//                                                         / _ \                                            //
+//                                            _   _  _   _( (_) |                                           //
+//                                           | | | || | | |\__, |                                           //
+//                                           | |_| || |_| |  / /                                            //
+//                                           | ._,_| \___/  /_/                                             //
+//                                           | |                                                            //
+//                                           |_|                                                            //
+
+// mu9_is_sorted_until.h
+//
+// Copyright (C) 2023 mu578. All rights reserved.
+//
+
+#include <mu0/mu0_definition.h>
+
+#ifndef MU9_IS_SORTED_UNTIL_H
+#define MU9_IS_SORTED_UNTIL_H 1
+
+MU0_BEGIN_CDECL
+
+#	define mu9_is_sorted_until1(_Tp, __first, __last, __d_result) \
+mu0_scope_begin                                                  \
+	_Tp * __mu9_is_sorted_until1__i__;                            \
+	__d_result = __last;                                          \
+	if (__first != __last) {                                      \
+		__mu9_is_sorted_until1__i__ = __first;                     \
+		while (++__mu9_is_sorted_until1__i__ != __last) {          \
+			if (*__mu9_is_sorted_until1__i__ < *__first) {          \
+				__d_result = __mu9_is_sorted_until1__i__;            \
+				break;                                               \
+			}                                                       \
+			__first = __mu9_is_sorted_until1__i__;                  \
+		}                                                          \
+	}                                                             \
+mu0_scope_end
+
+#	define mu9_is_sorted_until2(_Tp, __first, __last, __comp_fn, __d_result) \
+mu0_scope_begin                                                             \
+	_Tp * __mu9_is_sorted_until2__i__;                                       \
+	__d_result = __last;                                                     \
+	if (__first != __last) {                                                 \
+		__mu9_is_sorted_until2__i__ = __first;                                \
+		while (++__mu9_is_sorted_until2__i__ != __last) {                     \
+			if (__comp_fn(*__mu9_is_sorted_until2__i__, *__first)) {           \
+				__d_result = __mu9_is_sorted_until2__i__;                       \
+				break;                                                          \
+			}                                                                  \
+			__first = __mu9_is_sorted_until2__i__;                             \
+		}                                                                     \
+	}                                                                        \
+mu0_scope_end
+
+#	define mu9_is_sorted_until3(_Tp, __first, __last, __sort_fn, __ctx, __d_result) \
+mu0_scope_begin                                                                    \
+	_Tp * __mu9_is_sorted_until3__i__;                                              \
+	__d_result = __last;                                                            \
+	if (__first != __last) {                                                        \
+		__mu9_is_sorted_until3__i__ = __first;                                       \
+		while (++__mu9_is_sorted_until3__i__ != __last) {                            \
+			if (__sort_fn(*__mu9_is_sorted_until3__i__, *__first, __ctx)) {           \
+				__d_result = __mu9_is_sorted_until3__i__;                              \
+				break;                                                                 \
+			}                                                                         \
+			__first = __mu9_is_sorted_until3__i__;                                    \
+		}                                                                            \
+	}                                                                               \
+mu0_scope_end
+
+MU0_END_CDECL
+
+#endif /* !MU9_IS_SORTED_UNTIL_H */
+
+/* EOF */
