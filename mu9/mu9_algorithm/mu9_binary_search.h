0372a950e212e294caa2bcbbbe24b8f984f91387
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-25 08:43:29 -0800

    cleanups

--- mu9/mu9_algorithm/mu9_binary_search.h
+++ mu9/mu9_algorithm/mu9_binary_search.h
@@ -43,14 +43,14 @@ mu0_scope_begin                                                                 
 	);                                                                                                 \
 mu0_scope_end
 
-#	define mu9_binary_search3(_Tp, __first, __last, __value, __sort_op, __ctx, __d_result)                    \
-mu0_scope_begin                                                                                              \
-	_Tp * __mu9_lower_bound2__d__;                                                                            \
-	mu9_lower_bound3(__Tp, __first, __last, __value, __sort_op, __ctx, __mu9_lower_bound2__d__);              \
+#	define mu9_binary_search3(_Tp, __first, __last, __value, __sort_op, __ctx, __d_result)                         \
+mu0_scope_begin                                                                                                   \
+	_Tp * __mu9_lower_bound2__d__;                                                                                 \
+	mu9_lower_bound3(__Tp, __first, __last, __value, __sort_op, __ctx, __mu9_lower_bound2__d__);                   \
 	__d_result = ((__mu9_lower_bound2__d__ != __last && !__sort_op(_Tp, __value, *__mu9_lower_bound2__d__, __ctx)) \
-		? mu0_true                                                                                             \
-		: mu0_false                                                                                            \
-	);                                                                                                        \
+		? mu0_true                                                                                                  \
+		: mu0_false                                                                                                 \
+	);                                                                                                             \
 mu0_scope_end
 
 MU0_END_CDECL
