ed39e79c56d0947824f1e1c0d1ede3edd206599b
mu578 <19998532+mu578@users.noreply.github.com>
2023-02-24 18:23:11 -0800

    cleanups

--- misc/linker-main.c
+++ misc/linker-main.c
@@ -15,8 +15,15 @@ int main(int argc, const char * argv[])
 		,  2.300f, 1.300f
 	};
 
-	mu0_vsint32_t it0_first; 
-	
+	mu0_vsint32_t it0_first;
+
+	const mu0_vuint32_t it1_first = mu9_const_begin(mu0_uint32_t, v1);
+	const mu0_vuint32_t it1_last  = mu9_const_end  (mu0_uint32_t, v1, 10);
+	const mu0_vfp32_t   it2_first = mu9_const_begin(mu0_fp32_t, v2);
+
+	mu0_unused(argc);
+	mu0_unused(argv);
+
 	it0_first = mu9_begin(mu0_sint32_t, v0);
 	mu9_advance_n(mu0_sint32_t, it0_first, 2);
 	mu0_console_log("%d \n", *it0_first);
@@ -24,10 +31,6 @@ int main(int argc, const char * argv[])
 	it0_first = mu9_end(mu0_sint32_t, v0, 3);
 	mu9_advance_n(mu0_sint32_t, it0_first, -2);
 	mu0_console_log("%d \n", *it0_first);
-
-	const mu0_vuint32_t it1_first = mu9_const_begin(mu0_uint32_t, v1);
-	const mu0_vuint32_t it1_last  = mu9_const_end  (mu0_uint32_t, v1, 10);
-	const mu0_vfp32_t   it2_first = mu9_const_begin(mu0_fp32_t, v2);
 
 	mu9_argument_sort1(mu0_uint32_t, it1_first, it1_last, 0, it2_first);
 
