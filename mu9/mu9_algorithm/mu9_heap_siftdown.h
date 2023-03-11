0ff52649f767383a10e5c78c2df1923885f5ec16
mu578 <19998532+mu578@users.noreply.github.com>
2023-03-10 22:57:45 -0800

    heap_siftup

--- mu9/mu9_algorithm/mu9_heap_siftdown.h
+++ mu9/mu9_algorithm/mu9_heap_siftdown.h
@@ -22,72 +22,74 @@
 
 MU0_BEGIN_CDECL
 
-#	define mu9_heap_siftdown1(_Tp, __first, __len, __start, __comp_fn)                                        \
-mu0_scope_begin                                                                                              \
-	_Tp *          __mu9_heap_siftdown1__i__;                                                                 \
-	_Tp            __mu9_heap_siftdown1__v__;                                                                 \
-	mu0_distance_t __mu9_heap_siftdown1__c__ = mu9_const_distance(_Tp, __first, __start);                     \
-	mu0_distance_t __mu9_heap_siftdown1__d__;                                                                 \
-	if (!(__len < 2 || (__len - 2) / 2 < __mu9_heap_siftdown1__c__)) {                                        \
-		__mu9_heap_siftdown1__c__ = mu0_const_distance(2) * __mu9_heap_siftdown1__c__ + mu0_const_distance(1); \
-		__mu9_heap_siftdown1__i__ = mu9_next_n(__first, __mu9_heap_siftdown1__c__);                            \
-		if ((__mu9_heap_siftdown1__c__ + 1) < __len && __comp_fn(_Tp                                           \
-			, *(__mu9_heap_siftdown1__i__)                                                                      \
-			, *(__mu9_heap_siftdown1__i__ + mu0_const_distance(1))                                              \
-		)) { ++__mu9_heap_siftdown1__i__; ++__mu9_heap_siftdown1__c__; }                                       \
-		if (!__comp_fn(_Tp, *__mu9_heap_siftdown1__i__, *__start)) {                                           \
-			__mu9_heap_siftdown1__v__ = *__start;                                                               \
-			do {                                                                                                \
-				*__start                  = *__mu9_heap_siftdown1__i__;                                          \
-				__start                   =  __mu9_heap_siftdown1__i__;                                          \
-				__mu9_heap_siftdown1__d__ = (__len - mu0_const_distance(2)) / mu0_const_distance(2);             \
-				if (__mu9_heap_siftdown1__d__ < __mu9_heap_siftdown1__c__) {                                     \
-					break;                                                                                        \
-				}                                                                                                \
-				__mu9_heap_siftdown1__c__ = mu0_const_distance(2) * __mu9_heap_siftdown1__c__ + 1;               \
-				__mu9_heap_siftdown1__i__ = __first + __mu9_heap_siftdown1__c__;                                 \
-				if ((__mu9_heap_siftdown1__c__ + mu0_const_distance(1)) < __len && __comp_fn(_Tp                 \
-					, *(__mu9_heap_siftdown1__i__)                                                                \
-					, *(__mu9_heap_siftdown1__i__ + mu0_const_distance(1))                                        \
-				)) { ++__mu9_heap_siftdown1__i__; ++__mu9_heap_siftdown1__c__; }                                 \
-			} while (!__comp_fn(_Tp, *__mu9_heap_siftdown1__i__, __mu9_heap_siftdown1__v__));                   \
-			*__start                  = __mu9_heap_siftdown1__v__;                                              \
-		}                                                                                                      \
-	}                                                                                                         \
+#	define mu9_heap_siftdown1(_Tp, __first, __len, __start, __comp_fn)                                                \
+mu0_scope_begin                                                                                                      \
+	_Tp *                __mu9_heap_siftdown1__i__;                                                                   \
+	_Tp                  __mu9_heap_siftdown1__v__;                                                                   \
+	      mu0_distance_t __mu9_heap_siftdown1__c__ = mu9_const_distance(_Tp, __first, __start);                       \
+	const mu0_distance_t __mu9_heap_siftdown1__l__ = mu0_const_distance(__len);                                       \
+	      mu0_distance_t __mu9_heap_siftdown1__d__;                                                                   \
+	if (!(__mu9_heap_siftdown1__l__ < 2 || (__mu9_heap_siftdown1__l__ - 2) / 2 < __mu9_heap_siftdown1__c__)) {        \
+		__mu9_heap_siftdown1__c__ = mu0_const_distance(2) * __mu9_heap_siftdown1__c__ + mu0_const_distance(1);         \
+		__mu9_heap_siftdown1__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftdown1__c__);                               \
+		if ((__mu9_heap_siftdown1__c__ + 1) < __mu9_heap_siftdown1__l__ && __comp_fn(_Tp                               \
+			, *(__mu9_heap_siftdown1__i__)                                                                              \
+			, *(__mu9_heap_siftdown1__i__ + mu0_const_distance(1))                                                      \
+		)) { ++__mu9_heap_siftdown1__i__; ++__mu9_heap_siftdown1__c__; }                                               \
+		if (!__comp_fn(_Tp, *__mu9_heap_siftdown1__i__, *__start)) {                                                   \
+			__mu9_heap_siftdown1__v__ = *__start;                                                                       \
+			do {                                                                                                        \
+				*__start                  = *__mu9_heap_siftdown1__i__;                                                  \
+				__start                   =  __mu9_heap_siftdown1__i__;                                                  \
+				__mu9_heap_siftdown1__d__ = (__mu9_heap_siftdown1__l__ - mu0_const_distance(2)) / mu0_const_distance(2); \
+				if (__mu9_heap_siftdown1__d__ < __mu9_heap_siftdown1__c__) {                                             \
+					break;                                                                                                \
+				}                                                                                                        \
+				__mu9_heap_siftdown1__c__ = mu0_const_distance(2) * __mu9_heap_siftdown1__c__ + 1;                       \
+				__mu9_heap_siftdown1__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftdown1__c__);                         \
+				if ((__mu9_heap_siftdown1__c__ + mu0_const_distance(1)) < __mu9_heap_siftdown1__l__ && __comp_fn(_Tp     \
+					, *(__mu9_heap_siftdown1__i__)                                                                        \
+					, *(__mu9_heap_siftdown1__i__ + mu0_const_distance(1))                                                \
+				)) { ++__mu9_heap_siftdown1__i__; ++__mu9_heap_siftdown1__c__; }                                         \
+			} while (!__comp_fn(_Tp, *__mu9_heap_siftdown1__i__, __mu9_heap_siftdown1__v__));                           \
+			*__start                  = __mu9_heap_siftdown1__v__;                                                      \
+		}                                                                                                              \
+	}                                                                                                                 \
 mu0_scope_end
 
-#	define mu9_heap_siftdown2(_Tp, __first, __len, __start, __comp_fn, __move_op)                             \
-mu0_scope_begin                                                                                              \
-	_Tp *          __mu9_heap_siftdown2__i__;                                                                 \
-	_Tp            __mu9_heap_siftdown2__v__;                                                                 \
-	mu0_distance_t __mu9_heap_siftdown2__c__ = mu9_const_distance(_Tp, __first, __start);                     \
-	mu0_distance_t __mu9_heap_siftdown2__d__;                                                                 \
-	if (!(__len < 2 || (__len - 2) / 2 < __mu9_heap_siftdown2__c__)) {                                        \
-		__mu9_heap_siftdown2__c__ = mu0_const_distance(2) * __mu9_heap_siftdown2__c__ + mu0_const_distance(1); \
-		__mu9_heap_siftdown2__i__ = mu9_next_n(__first, __mu9_heap_siftdown2__c__);                            \
-		if ((__mu9_heap_siftdown2__c__ + 1) < __len && __comp_fn(_Tp                                           \
-			, *(__mu9_heap_siftdown2__i__)                                                                      \
-			, *(__mu9_heap_siftdown2__i__ + mu0_const_distance(1))                                              \
-		)) { ++__mu9_heap_siftdown2__i__; ++__mu9_heap_siftdown2__c__; }                                       \
-		if (!__comp_fn(_Tp, *__mu9_heap_siftdown2__i__, *__start)) {                                           \
-			__move_op(_Tp, __mu9_heap_siftdown2__v__, *__start);                                                \
-			do {                                                                                                \
-				__move_op(_Tp, *__start, *__mu9_heap_siftdown2__i__);                                            \
-				__start                   =  __mu9_heap_siftdown2__i__;                                          \
-				__mu9_heap_siftdown2__d__ = (__len - mu0_const_distance(2)) / mu0_const_distance(2);             \
-				if (__mu9_heap_siftdown2__d__ < __mu9_heap_siftdown2__c__) {                                     \
-					break;                                                                                        \
-				}                                                                                                \
-				__mu9_heap_siftdown2__c__ = mu0_const_distance(2) * __mu9_heap_siftdown2__c__ + 1;               \
-				__mu9_heap_siftdown2__i__ = __first + __mu9_heap_siftdown2__c__;                                 \
-				if ((__mu9_heap_siftdown2__c__ + mu0_const_distance(1)) < __len && __comp_fn(_Tp                 \
-					, *(__mu9_heap_siftdown2__i__)                                                                \
-					, *(__mu9_heap_siftdown2__i__ + mu0_const_distance(1))                                        \
-				)) { ++__mu9_heap_siftdown2__i__; ++__mu9_heap_siftdown2__c__; }                                 \
-			} while (!__comp_fn(_Tp, *__mu9_heap_siftdown2__i__, __mu9_heap_siftdown2__v__));                   \
-			__move_op(_Tp, *__start, __mu9_heap_siftdown2__v__);                                                \
-		}                                                                                                      \
-	}                                                                                                         \
+#	define mu9_heap_siftdown2(_Tp, __first, __len, __start, __comp_fn, __move_op)                                     \
+mu0_scope_begin                                                                                                      \
+	_Tp *                __mu9_heap_siftdown2__i__;                                                                   \
+	_Tp                  __mu9_heap_siftdown2__v__;                                                                   \
+	      mu0_distance_t __mu9_heap_siftdown2__c__ = mu9_const_distance(_Tp, __first, __start);                       \
+	const mu0_distance_t __mu9_heap_siftdown2__l__ = mu0_const_distance(__len);                                       \
+	      mu0_distance_t __mu9_heap_siftdown2__d__;                                                                   \
+	if (!(__mu9_heap_siftdown2__l__ < 2 || (__mu9_heap_siftdown2__l__ - 2) / 2 < __mu9_heap_siftdown2__c__)) {        \
+		__mu9_heap_siftdown2__c__ = mu0_const_distance(2) * __mu9_heap_siftdown2__c__ + mu0_const_distance(1);         \
+		__mu9_heap_siftdown2__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftdown2__c__);                               \
+		if ((__mu9_heap_siftdown2__c__ + 1) < __mu9_heap_siftdown2__l__ && __comp_fn(_Tp                               \
+			, *(__mu9_heap_siftdown2__i__)                                                                              \
+			, *(__mu9_heap_siftdown2__i__ + mu0_const_distance(1))                                                      \
+		)) { ++__mu9_heap_siftdown2__i__; ++__mu9_heap_siftdown2__c__; }                                               \
+		if (!__comp_fn(_Tp, *__mu9_heap_siftdown2__i__, *__start)) {                                                   \
+			__move_op(_Tp, __mu9_heap_siftdown2__v__, *__start);                                                        \
+			do {                                                                                                        \
+				__move_op(_Tp, *__start, *__mu9_heap_siftdown2__i__);                                                    \
+				__start                   =  __mu9_heap_siftdown2__i__;                                                  \
+				__mu9_heap_siftdown2__d__ = (__mu9_heap_siftdown2__l__ - mu0_const_distance(2)) / mu0_const_distance(2); \
+				if (__mu9_heap_siftdown2__d__ < __mu9_heap_siftdown2__c__) {                                             \
+					break;                                                                                                \
+				}                                                                                                        \
+				__mu9_heap_siftdown2__c__ = mu0_const_distance(2) * __mu9_heap_siftdown2__c__ + 1;                       \
+				__mu9_heap_siftdown2__i__ = mu9_next_n(_Tp, __first + __mu9_heap_siftdown2__c__);                        \
+				if ((__mu9_heap_siftdown2__c__ + mu0_const_distance(1)) < __mu9_heap_siftdown2__l__ && __comp_fn(_Tp     \
+					, *(__mu9_heap_siftdown2__i__)                                                                        \
+					, *(__mu9_heap_siftdown2__i__ + mu0_const_distance(1))                                                \
+				)) { ++__mu9_heap_siftdown2__i__; ++__mu9_heap_siftdown2__c__; }                                         \
+			} while (!__comp_fn(_Tp, *__mu9_heap_siftdown2__i__, __mu9_heap_siftdown2__v__));                           \
+			__move_op(_Tp, *__start, __mu9_heap_siftdown2__v__);                                                        \
+		}                                                                                                              \
+	}                                                                                                                 \
 mu0_scope_end
 
 MU0_END_CDECL
--- mu9/mu9_algorithm/mu9_heap_siftup.h
+++ mu9/mu9_algorithm/mu9_heap_siftup.h
@@ -29,7 +29,7 @@ mu0_scope_begin                                                                 
 	mu0_distance_t __mu9_heap_siftup1__c__ = mu0_const_distance(__len);                                           \
 	if (__mu9_heap_siftup1__c__ > mu0_const_distance(1)) {                                                        \
 		__mu9_heap_siftup1__c__ = (__mu9_heap_siftup1__c__ - mu0_const_distance(2)) / mu0_const_distance(2);       \
-		__mu9_heap_siftup1__i__ = mu9_next_n(__first, __mu9_heap_siftup1__c__);                                    \
+		__mu9_heap_siftup1__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftup1__c__);                               \
 		if (__comp_fn(_Tp, *__mu9_heap_siftup1__i__, *--__last)) {                                                 \
 			__mu9_heap_siftup1__v__ = *__last;                                                                      \
 			do {                                                                                                    \
@@ -39,7 +39,7 @@ mu0_scope_begin                                                                 
 					break;                                                                                            \
 				}                                                                                                    \
 				__mu9_heap_siftup1__c__ = (__mu9_heap_siftup1__c__ - mu0_const_distance(1)) / mu0_const_distance(2); \
-				__mu9_heap_siftup1__i__ = mu9_next_n(__first, __mu9_heap_siftup1__c__);                              \
+				__mu9_heap_siftup1__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftup1__c__);                         \
 			} while (__comp_fn(_Tp, *__mu9_heap_siftup1__i__, __mu9_heap_siftup1__v__));                            \
 			*__last = __mu9_heap_siftup1__v__;                                                                      \
 		}                                                                                                          \
@@ -53,7 +53,7 @@ mu0_scope_begin                                                                 
 	mu0_distance_t __mu9_heap_siftup2__c__ = mu0_const_distance(__len);                                           \
 	if (__mu9_heap_siftup2__c__ > mu0_const_distance(1)) {                                                        \
 		__mu9_heap_siftup2__c__ = (__mu9_heap_siftup2__c__ - mu0_const_distance(2)) / mu0_const_distance(2);       \
-		__mu9_heap_siftup2__i__ = mu9_next_n(__first, __mu9_heap_siftup2__c__);                                    \
+		__mu9_heap_siftup2__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftup2__c__);                               \
 		if (__comp_fn(_Tp, *__mu9_heap_siftup2__i__, *--__last)) {                                                 \
 			__move_op(_Tp, __mu9_heap_siftup2__v__, *__last);                                                       \
 			do {                                                                                                    \
@@ -63,7 +63,7 @@ mu0_scope_begin                                                                 
 					break;                                                                                            \
 				}                                                                                                    \
 				__mu9_heap_siftup2__c__ = (__mu9_heap_siftup2__c__ - mu0_const_distance(1)) / mu0_const_distance(2); \
-				__mu9_heap_siftup2__i__ = mu9_next_n(__first, __mu9_heap_siftup2__c__);                              \
+				__mu9_heap_siftup2__i__ = mu9_next_n(_Tp, __first, __mu9_heap_siftup2__c__);                         \
 			} while (__comp_fn(_Tp, *__mu9_heap_siftup2__i__, __mu9_heap_siftup2__v__));                            \
 			__move_op(_Tp, *__last, __mu9_heap_siftup2__v__);                                                       \
 		}                                                                                                          \
