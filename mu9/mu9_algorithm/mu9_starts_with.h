258b4efde03fe7c2a62217485727395993be3889
mu578 <19998532+mu578@users.noreply.github.com>
2023-04-11 17:36:20 -0700

    cleanups

--- mu9/mu9_algorithm/mu9_starts_with.h
+++ mu9/mu9_algorithm/mu9_starts_with.h
@@ -22,38 +22,38 @@
 
 MU0_BEGIN_CDECL
 
-#	define mu9_starts_with1(_Tp, __first1, __last1, __first2, __last2, __d_result)                 \
-mu0_scope_begin                                                                                   \
-	      mu0_bool_t     __mu9_mu9_starts_with1__x__ = mu0_false;                                  \
-	const mu0_distance_t __mu9_mu9_starts_with1__n__ = mu9_const_distance(_Tp, __first1, __last1); \
-	const mu0_distance_t __mu9_mu9_starts_with1__k__ = mu9_const_distance(_Tp, __first2, __last2); \
-	if (__mu9_mu9_starts_with1__k__ >= __mu9_mu9_starts_with1__n__) {                              \
-		__mu9_mu9_starts_with1__x__ = mu0_true;                                                     \
-		while (__first1 != __last1) {                                                               \
-			if (!(*__first1++ == *__first2++)) {                                                     \
-				__mu9_mu9_starts_with1__x__ = mu0_false;                                              \
-				break;                                                                                \
-			}                                                                                        \
-		}                                                                                           \
-	}                                                                                              \
-	__d_result = __mu9_mu9_starts_with1__x__;                                                      \
+#	define mu9_starts_with1(_Tp, __first1, __last1, __first2, __last2, __d_result)             \
+mu0_scope_begin                                                                               \
+	      mu0_bool_t     __mu9_starts_with1__x__ = mu0_false;                                  \
+	const mu0_distance_t __mu9_starts_with1__n__ = mu9_const_distance(_Tp, __first1, __last1); \
+	const mu0_distance_t __mu9_starts_with1__k__ = mu9_const_distance(_Tp, __first2, __last2); \
+	if (__mu9_starts_with1__k__ >= __mu9_starts_with1__n__) {                                  \
+		__mu9_starts_with1__x__ = mu0_true;                                                     \
+		while (__first1 != __last1) {                                                           \
+			if (!(*__first1++ == *__first2++)) {                                                 \
+				__mu9_starts_with1__x__ = mu0_false;                                              \
+				break;                                                                            \
+			}                                                                                    \
+		}                                                                                       \
+	}                                                                                          \
+	__d_result = __mu9_starts_with1__x__;                                                      \
 mu0_scope_end
 
-#	define mu9_starts_with2(_Tp, __first1, __last1, __first2, __last2, __binary_pred, __d_result)  \
-mu0_scope_begin                                                                                   \
-	      mu0_bool_t     __mu9_mu9_starts_with2__x__ = mu0_false;                                  \
-	const mu0_distance_t __mu9_mu9_starts_with2__n__ = mu9_const_distance(_Tp, __first1, __last1); \
-	const mu0_distance_t __mu9_mu9_starts_with2__k__ = mu9_const_distance(_Tp, __first2, __last2); \
-	if (__mu9_mu9_starts_with2__k__ >= __mu9_mu9_starts_with2__n__) {                              \
-		__mu9_mu9_starts_with2__x__ = mu0_true;                                                     \
-		while (__first1 != __last1) {                                                               \
-			if (!__binary_pred(_Tp, *__first1++, *__first2++)) {                                     \
-				__mu9_mu9_starts_with2__x__ = mu0_false;                                              \
-				break;                                                                                \
-			}                                                                                        \
-		}                                                                                           \
-	}                                                                                              \
-	__d_result = __mu9_mu9_starts_with2__x__;                                                      \
+#	define mu9_starts_with2(_Tp, __first1, __last1, __first2, __last2, __binary_pred, __d_result) \
+mu0_scope_begin                                                                                  \
+	      mu0_bool_t     __mu9_starts_with2__x__ = mu0_false;                                     \
+	const mu0_distance_t __mu9_starts_with2__n__ = mu9_const_distance(_Tp, __first1, __last1);    \
+	const mu0_distance_t __mu9_starts_with2__k__ = mu9_const_distance(_Tp, __first2, __last2);    \
+	if (__mu9_starts_with2__k__ >= __mu9_starts_with2__n__) {                                     \
+		__mu9_starts_with2__x__ = mu0_true;                                                        \
+		while (__first1 != __last1) {                                                              \
+			if (!__binary_pred(_Tp, *__first1++, *__first2++)) {                                    \
+				__mu9_starts_with2__x__ = mu0_false;                                                 \
+				break;                                                                               \
+			}                                                                                       \
+		}                                                                                          \
+	}                                                                                             \
+	__d_result = __mu9_starts_with2__x__;                                                         \
 mu0_scope_end
 
 MU0_END_CDECL
--- mu9/mu9_algorithm/mu9_unique.h
+++ mu9/mu9_algorithm/mu9_unique.h
@@ -22,34 +22,34 @@
 
 MU0_BEGIN_CDECL
 
-#	define mu9_unique1(_Tp, __first, __last, __d_result)                                       \
-mu0_scope_begin                                                                               \
-	_Tp * __mu9_mu9_unique1__i__ = __last;                                                     \
-	if (__first != __mu9_mu9_unique1__i__) {                                                   \
-		__mu9_mu9_unique1__i__ = __first;                                                       \
-		while (++__first != __last) {                                                           \
-			if (!(*__mu9_mu9_unique1__i__ == *__first) && ++__mu9_mu9_unique1__i__ != __first) { \
-				*__mu9_mu9_unique1__i__ = *__first;                                               \
-			}                                                                                    \
-		}                                                                                       \
-		++__mu9_mu9_unique1__i__;                                                               \
-	}                                                                                          \
-	__d_result = __mu9_mu9_unique1__i__; \
+#	define mu9_unique1(_Tp, __first, __last, __d_result)                               \
+mu0_scope_begin                                                                       \
+	_Tp * __mu9_unique1__i__ = __last;                                                 \
+	if (__first != __mu9_unique1__i__) {                                               \
+		__mu9_unique1__i__ = __first;                                                   \
+		while (++__first != __last) {                                                   \
+			if (!(*__mu9_unique1__i__ == *__first) && ++__mu9_unique1__i__ != __first) { \
+				*__mu9_unique1__i__ = *__first;                                           \
+			}                                                                            \
+		}                                                                               \
+		++__mu9_unique1__i__;                                                           \
+	}                                                                                  \
+	__d_result = __mu9_unique1__i__;                                                   \
 mu0_scope_end
 
-#	define mu9_unique2(_Tp, __first, __last, __d_result, __binary_pred)                                        \
-mu0_scope_begin                                                                                               \
-	_Tp * __mu9_mu9_unique2__i__ = __last;                                                                     \
-	if (__first != __mu9_mu9_unique2__i__) {                                                                   \
-		__mu9_mu9_unique2__i__ = __first;                                                                       \
-		while (++__first != __last) {                                                                           \
-			if (!__binary_pred(_Tp, *__mu9_mu9_unique2__i__, *__first) && ++__mu9_mu9_unique2__i__ != __first) { \
-				*__mu9_mu9_unique2__i__ = *__first;                                                               \
-			}                                                                                                    \
-		}                                                                                                       \
-		++__mu9_mu9_unique2__i__;                                                                               \
-	}                                                                                                          \
-	__d_result = __mu9_mu9_unique2__i__; \
+#	define mu9_unique2(_Tp, __first, __last, __d_result, __binary_pred)                                \
+mu0_scope_begin                                                                                       \
+	_Tp * __mu9_unique2__i__ = __last;                                                                 \
+	if (__first != __mu9_unique2__i__) {                                                               \
+		__mu9_unique2__i__ = __first;                                                                   \
+		while (++__first != __last) {                                                                   \
+			if (!__binary_pred(_Tp, *__mu9_unique2__i__, *__first) && ++__mu9_unique2__i__ != __first) { \
+				*__mu9_unique2__i__ = *__first;                                                           \
+			}                                                                                            \
+		}                                                                                               \
+		++__mu9_unique2__i__;                                                                           \
+	}                                                                                                  \
+	__d_result = __mu9_unique2__i__;                                                                   \
 mu0_scope_end
 
 #endif /* !MU9_UNIQUE_H */
