66799e928b61a137be879e82b339955cb224fb74
mu578 <19998532+mu578@users.noreply.github.com>
2023-04-15 12:15:40 -0700

    adding indirect move policy, let input iterator untouched

--- mu9/mu9_algorithm/mu9_move.h
+++ mu9/mu9_algorithm/mu9_move.h
@@ -48,12 +48,32 @@ mu0_scope_begin                                                                 
 	}                                                                            \
 mu0_scope_end
 
+#	define mu9_move_indirect_if(_Tp, __first, __last, __d_first, __unary_pred, __move_op)                                             \
+mu0_scope_begin                                                                                                                      \
+	_Tp * __mu9_move_indirect_if__i__ = __first;                                                                                      \
+	_Tp * __mu9_move_indirect_if__j__ = __last;                                                                                       \
+	_Tp * __mu9_move_indirect_if__k__ = __d_first;                                                                                    \
+	mu9_move_if(_Tp, __mu9_move_indirect_if__i__, __mu9_move_indirect_if__j__, __mu9_move_indirect_if__k__, __unary_pred, __move_op); \
+	__d_first                         = __mu9_move_indirect_if__k__;                                                                  \
+mu0_scope_end
+
 #	define mu9_move_to(_Tp, __first, __last, __d_first, __move_op, __d_result) \
 mu0_scope_begin                                                               \
 	for (; __first != __last; (void) ++__first, (void) ++__d_first) {          \
 		 __move_op(_Tp, *__d_first, *__first);                                  \
 	}                                                                          \
 	__d_result = __d_first;                                                    \
+mu0_scope_end
+
+#	define mu9_move_indirect_to(_Tp, __first, __last, __d_first, __move_op, __d_result)                                                              \
+mu0_scope_begin                                                                                                                                     \
+	_Tp * __mu9_move_indirect_to__i__ = __first;                                                                                                     \
+	_Tp * __mu9_move_indirect_to__j__ = __last;                                                                                                      \
+	_Tp * __mu9_move_indirect_to__k__ = __d_first;                                                                                                   \
+	_Tp * __mu9_move_indirect_to__r__;                                                                                                               \
+	mu9_move_to(_Tp, __mu9_move_indirect_to__i__, __mu9_move_indirect_to__j__, __mu9_move_indirect_to__k__, __move_op, __mu9_move_indirect_to__r__); \
+	__d_first                         = __mu9_move_indirect_to__k__;                                                                                 \
+	__d_result                        = __mu9_move_indirect_to__r__;                                                                                 \
 mu0_scope_end
 
 #	define mu9_move_to_if(_Tp, __first, __last, __d_first, __unary_pred, __move_op, __d_result) \
@@ -65,6 +85,17 @@ mu0_scope_begin                                                                 
 		}                                                                                        \
 	}                                                                                           \
 	__d_result = __d_first;                                                                     \
+mu0_scope_end
+
+#	define mu9_move_indirect_to_if(_Tp, __first, __last, __d_first, __unary_pred, __move_op, __d_result)                                                      \
+mu0_scope_begin                                                                                                                                              \
+	_Tp * __mu9_move_indirect_to_if__i__ = __first;                                                                                                           \
+	_Tp * __mu9_move_indirect_to_if__j__ = __last;                                                                                                            \
+	_Tp * __mu9_move_indirect_to_if__k__ = __d_first;                                                                                                         \
+	_Tp * __mu9_move_indirect_to_if__r__;                                                                                                                     \
+	mu9_move_to_if(_Tp, __mu9_move_indirect_to_if__i__, __mu9_move_indirect_to_if__j__, __mu9_move_indirect_to_if__k__, __unary_pred, __move_op, __d_result); \
+	__d_first                            = __mu9_move_indirect_to_if__k__;                                                                                    \
+	__d_result                           = __mu9_move_indirect_to_if__r__;                                                                                    \
 mu0_scope_end
 
 MU0_END_CDECL
