6d1d8c0bafa53552d15e64e40c4cc9dcf3b7f132
mu578 <19998532+mu578@users.noreply.github.com>
2023-04-24 20:38:10 -0700

    adding search_first similar to libc strpbrk

---
+++ mu9/mu9_character.h
@@ -0,0 +1,34 @@
+//
+// # -*- coding: utf-8, tab-width: 3 -*-
+
+//                                                          ___                                             //
+//                                                         / _ \                                            //
+//                                            _   _  _   _( (_) |                                           //
+//                                           | | | || | | |\__, |                                           //
+//                                           | |_| || |_| |  / /                                            //
+//                                           | ._,_| \___/  /_/                                             //
+//                                           | |                                                            //
+//                                           |_|                                                            //
+
+// mu9_character.h
+//
+// Copyright (C) 2023 mu578. All rights reserved.
+//
+
+#ifndef MU9_CHARACTER_H
+#define MU9_CHARACTER_H 1
+
+#	include <mu9/mu9_algorithm/mu9_contains.h>
+#	include <mu9/mu9_algorithm/mu9_ends_with.h>
+#	include <mu9/mu9_algorithm/mu9_search_accept.h>
+#	include <mu9/mu9_algorithm/mu9_search_first.h>
+#	include <mu9/mu9_algorithm/mu9_search_reject.h>
+#	include <mu9/mu9_algorithm/mu9_starts_with.h>
+
+MU0_BEGIN_CDECL
+
+MU0_END_CDECL
+
+#endif /* !MU9_CHARACTER_H */
+
+/* EOF */
