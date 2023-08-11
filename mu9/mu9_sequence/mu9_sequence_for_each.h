eae65f7c460d14b93afdd7f9b7e9b4783db22d71
mu578 <19998532+mu578@users.noreply.github.com>
2023-08-11 08:05:54 -0700

    cleanups

--- mu9/mu9_sequence/mu9_sequence_for_each.h
+++ mu9/mu9_sequence/mu9_sequence_for_each.h
@@ -15,7 +15,7 @@
 // Copyright (C) 2023 mu578. All rights reserved.
 //
 
-#include <mu0/mu0_integer.h>
+#include <mu9/mu9_sequence/mu9_sequence_iterator.h>
 
 #ifndef MU9_SEQUENCE_FOR_EACH_H
 #define MU9_SEQUENCE_FOR_EACH_H 1
@@ -23,30 +23,30 @@
 MU0_BEGIN_CDECL
 
 //#!
-//#! macro<_Tp>(_Tp &* __seq, _Tp<operator> * __next_node, __seq_next, __unary_fn) : void
+//#! macro<_Tp>(_Tp &* __seq, _Tp<operator> * __next_node, __unary_fn) : void
 //#!
-#	define mu9_sequence_for_each(_Tp, __seq, __next_node, __seq_next, __unary_fn) \
-mu0_scope_begin                                                                  \
-	_Tp ** __mu9_sequence_for_each__h__ = &(__seq);                               \
-	while (1) {                                                                   \
-		__unary_fn(Tp, *((*__mu9_sequence_for_each__h__)));                        \
-		__seq_next(Tp, *__mu9_sequence_for_each__h__, __next_node);                \
-		if (mu0_is_nullptr(*__mu9_sequence_for_each__h__)) {                       \
-			break;                                                                  \
-		}                                                                          \
-	}                                                                             \
+#	define mu9_sequence_for_each(_Tp, __seq, __next_node, __unary_fn)     \
+mu0_scope_begin                                                          \
+	_Tp ** __mu9_sequence_for_each__h__ = &(__seq);                       \
+	while (1) {                                                           \
+		__unary_fn(Tp, *((*__mu9_sequence_for_each__h__)));                \
+		mu9_sequence_next(Tp, *__mu9_sequence_for_each__h__, __next_node); \
+		if (mu0_is_nullptr(*__mu9_sequence_for_each__h__)) {               \
+			break;                                                          \
+		}                                                                  \
+	}                                                                     \
 mu0_scope_end
 
 //#!
-//#! macro<_Tp, _Uint>(_Tp &* __seq, _Tp<operator> * __next_node, __seq_next, const _Uint & __n, __unary_fn) : void
+//#! macro<_Tp, _Uint>(_Tp &* __seq, _Tp<operator> * __next_node, const _Uint & __n, __unary_fn) : void
 //#!
-#	define mu9_sequence_for_each_n(_Tp, __seq, __next_node, __seq_next, __n, __unary_fn)                        \
+#	define mu9_sequence_for_each_n(_Tp, __seq, __next_node, __n, __unary_fn)                                    \
 mu0_scope_begin                                                                                                \
 	mu0_distance_t __mu9_sequence_for_each_n__i__ = mu0_const_distance(0);                                      \
 	_Tp **         __mu9_sequence_for_each_n__h__ = &(__seq);                                                   \
 	for (; __mu9_sequence_for_each_n__i__ < mu0_const_distance(__n); (void) ++__mu9_sequence_for_each_n__i__) { \
 		__unary_fn(Tp, *((*__mu9_sequence_for_each_n__h__)));                                                    \
-		__seq_next(Tp, *__mu9_sequence_for_each_n__h__, __next_node);                                            \
+		mu9_sequence_next(Tp, *__mu9_sequence_for_each_n__h__, __next_node);                                     \
 		if (mu0_is_nullptr(*__mu9_sequence_for_each_n__h__)) {                                                   \
 			break;                                                                                                \
 		}                                                                                                        \
