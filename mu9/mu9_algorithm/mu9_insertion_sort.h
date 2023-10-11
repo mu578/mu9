fd909b651c8037d219fd4b5a9ae5639b8e0e90d7
mu578 <19998532+mu578@users.noreply.github.com>
2023-10-11 16:19:55 -0700

    fixing decay in insertion sort

--- mu9/mu9_algorithm/mu9_insertion_sort.h
+++ mu9/mu9_algorithm/mu9_insertion_sort.h
@@ -24,71 +24,77 @@
 
 MU0_BEGIN_CDECL
 
-#	define mu9_insertion_sort1(_Tp, __first, __last)                                 \
-mu0_scope_begin                                                                     \
-	_Tp * __mu9_insertion_sort1__i__ = __first;                                      \
-	_Tp * __mu9_insertion_sort1__j__;                                                \
-	for (; __mu9_insertion_sort1__i__ != __last; __mu9_insertion_sort1__i__++) {     \
-		__mu9_insertion_sort1__j__ = __mu9_insertion_sort1__i__;                      \
-		for (; __mu9_insertion_sort1__j__ != __first; __mu9_insertion_sort1__j__--) { \
-			if (                                                                       \
-				   *__mu9_insertion_sort1__j__                                          \
-				< *(__mu9_insertion_sort1__j__ - mu0_const_distance(1))                 \
-			) {                                                                        \
-				mu9_swap(_Tp                                                            \
-					,  *__mu9_insertion_sort1__j__                                       \
-					, *(__mu9_insertion_sort1__j__ - mu0_const_distance(1))              \
-				);                                                                      \
-			} else {                                                                   \
-				break;                                                                  \
-			}                                                                          \
-		}                                                                             \
-	}                                                                                \
+#	define mu9_insertion_sort1(_Tp, __first, __last)                                    \
+mu0_scope_begin                                                                        \
+	_Tp * __mu9_insertion_sort1__i__ = __first + mu0_const_distance(1);                 \
+	_Tp * __mu9_insertion_sort1__j__;                                                   \
+	if (__first != __last && __mu9_insertion_sort1__i__ != __last) {                    \
+		for (; __mu9_insertion_sort1__i__ != __last; __mu9_insertion_sort1__i__++) {     \
+			__mu9_insertion_sort1__j__ = __mu9_insertion_sort1__i__;                      \
+			for (; __mu9_insertion_sort1__j__ != __first; __mu9_insertion_sort1__j__--) { \
+				if (                                                                       \
+						*__mu9_insertion_sort1__j__                                          \
+					< *(__mu9_insertion_sort1__j__ - mu0_const_distance(1))                 \
+				) {                                                                        \
+					mu9_swap(_Tp                                                            \
+						,  *__mu9_insertion_sort1__j__                                       \
+						, *(__mu9_insertion_sort1__j__ - mu0_const_distance(1))              \
+					);                                                                      \
+				} else {                                                                   \
+					break;                                                                  \
+				}                                                                          \
+			}                                                                             \
+		}                                                                                \
+	}                                                                                   \
 mu0_scope_end
 
-#	define mu9_insertion_sort2(_Tp, __first, __last, __comp_fn)                      \
-mu0_scope_begin                                                                     \
-	_Tp * __mu9_insertion_sort2__i__ = __first;                                      \
-	_Tp * __mu9_insertion_sort2__j__;                                                \
-	for (; __mu9_insertion_sort2__i__ != __last; __mu9_insertion_sort2__i__++) {     \
-		__mu9_insertion_sort2__j__ = __mu9_insertion_sort2__i__;                      \
-		for (; __mu9_insertion_sort2__j__ != __first; __mu9_insertion_sort2__j__--) { \
-			if (__comp_fn(_Tp                                                          \
-				, *__mu9_insertion_sort2__j__                                           \
-				, *(__mu9_insertion_sort2__j__ - mu0_const_distance(1))                 \
-			)) {                                                                       \
-				mu9_swap(_Tp                                                            \
-					,  *__mu9_insertion_sort2__j__                                       \
-					, *(__mu9_insertion_sort2__j__ - mu0_const_distance(1))              \
-				);                                                                      \
-			} else {                                                                   \
-				break;                                                                  \
-			}                                                                          \
-		}                                                                             \
-	}                                                                                \
+#	define mu9_insertion_sort2(_Tp, __first, __last, __comp_fn)                         \
+mu0_scope_begin                                                                        \
+	_Tp * __mu9_insertion_sort2__i__ = __first + mu0_const_distance(1);                 \
+	_Tp * __mu9_insertion_sort2__j__;                                                   \
+	if (__first != __last && __mu9_insertion_sort2__i__ != __last) {                    \
+		for (; __mu9_insertion_sort2__i__ != __last; ++__mu9_insertion_sort2__i__) {     \
+			__mu9_insertion_sort2__j__ = __mu9_insertion_sort2__i__;                      \
+			for (; __mu9_insertion_sort2__j__ != __first; __mu9_insertion_sort2__j__--) { \
+				if (__comp_fn(_Tp                                                          \
+					, *__mu9_insertion_sort2__j__                                           \
+					, *(__mu9_insertion_sort2__j__ - mu0_const_distance(1))                 \
+				)) {                                                                       \
+					mu9_swap(_Tp                                                            \
+						,  *__mu9_insertion_sort2__j__                                       \
+						, *(__mu9_insertion_sort2__j__ - mu0_const_distance(1))              \
+					);                                                                      \
+				} else {                                                                   \
+					break;                                                                  \
+				}                                                                          \
+			}                                                                             \
+		}                                                                                \
+	}                                                                                   \
 mu0_scope_end
 
-#	define mu9_insertion_sort3(_Tp, __first, __last, __sort_op, __ctx)               \
-mu0_scope_begin                                                                     \
-	_Tp * __mu9_insertion_sort3__i__ = __first;                                      \
-	_Tp * __mu9_insertion_sort3__j__;                                                \
-	for (; __mu9_insertion_sort3__i__ != __last; __mu9_insertion_sort3__i__++) {     \
-		__mu9_insertion_sort3__j__ = __mu9_insertion_sort3__i__;                      \
-		for (; __mu9_insertion_sort3__j__ != __first; __mu9_insertion_sort3__j__--) { \
-			if (__sort_op(_Tp                                                          \
-				, *__mu9_insertion_sort3__j__                                           \
-				, *(__mu9_insertion_sort3__j__ - mu0_const_distance(1))                 \
-				, __ctx                                                                 \
-			)) {                                                                       \
-				mu9_swap(_Tp                                                            \
-					,  *__mu9_insertion_sort3__j__                                       \
-					, *(__mu9_insertion_sort3__j__ - mu0_const_distance(1))              \
-				);                                                                      \
-			} else {                                                                   \
-				break;                                                                  \
-			}                                                                          \
-		}                                                                             \
-	}                                                                                \
+#	define mu9_insertion_sort3(_Tp, __first, __last, __sort_op, __ctx)                  \
+mu0_scope_begin                                                                        \
+	_Tp * __mu9_insertion_sort3__i__ = __first + mu0_const_distance(1);                 \
+	_Tp * __mu9_insertion_sort3__j__;                                                   \
+	if (__first != __last && __mu9_insertion_sort3__i__ != __last) {                    \
+		for (; __mu9_insertion_sort3__i__ != __last; __mu9_insertion_sort3__i__++) {     \
+			__mu9_insertion_sort3__j__ = __mu9_insertion_sort3__i__;                      \
+			for (; __mu9_insertion_sort3__j__ != __first; __mu9_insertion_sort3__j__--) { \
+				if (__sort_op(_Tp                                                          \
+					, *__mu9_insertion_sort3__j__                                           \
+					, *(__mu9_insertion_sort3__j__ - mu0_const_distance(1))                 \
+					, __ctx                                                                 \
+				)) {                                                                       \
+					mu9_swap(_Tp                                                            \
+						,  *__mu9_insertion_sort3__j__                                       \
+						, *(__mu9_insertion_sort3__j__ - mu0_const_distance(1))              \
+					);                                                                      \
+				} else {                                                                   \
+					break;                                                                  \
+				}                                                                          \
+			}                                                                             \
+		}                                                                                \
+	}                                                                                   \
 mu0_scope_end
 
 #	define mu9_insertion_sort4(_Tp, __first, __last)                             \
