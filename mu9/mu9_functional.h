257c7465433c2a663c4fd8d72ba9bbd623a23edb
mu578 <19998532+mu578@users.noreply.github.com>
2023-10-11 15:52:01 -0700

    adding comment header everywhere... todo

--- mu9/mu9_functional.h
+++ mu9/mu9_functional.h
@@ -23,7 +23,7 @@
 MU0_BEGIN_CDECL
 
 //#!
-//#! @deprecated macro<_Tp>(_Tp & __a, _Tp & __b) : _Tp
+//#! macro{{binary_operation}}<_Tp>(_Tp & __a, _Tp & __b) : _Tp
 //#!
 #	define mu9_plus(_Tp, __a, __b)                   ((__a) + (__b))
 #	define mu9_minus(_Tp, __a, __b)                  ((__a) - (__b))
@@ -31,18 +31,22 @@ MU0_BEGIN_CDECL
 #	define mu9_divides(_Tp, __a, __b)                ((__a) / (__b))
 
 //#!
-//#! macro<_Tp>(_Tp & __a, _Tp & __b) : _Tp
+//#! macro{{binary_operation}}<_Tp>(_Tp & __a, _Tp & __b) : _Tp
 //#!
 #	define mu9_modulus(_Tp, __a, __b)                ((__a) % (__b))
-#	define mu9_negate(_Tp, __a)                      -(__a)
 
 //#!
-//#! macro<_Tp>(_Tp __x) : _Tp
+//#! macro{{unary_operation}}<_Tp>(_Tp & __x) : _Tp
+//#!
+#	define mu9_negate(_Tp, __x)                      -(__x)
+
+//#!
+//#! macro{{unary_operation}}<_Tp>(_Tp __x) : _Tp
 //#!
 #	define mu9_initialize(_Tp, __x)                  mu8_ini(_Tp, __x)
 
 //#!
-//#! macro<_Tp>(_Tp & __a, _Tp & __b) : _Tp
+//#! macro{{binary_operation}}<_Tp>(_Tp & __a, _Tp & __b) : _Tp
 //#!
 #	define mu9_addition(_Tp, __a, __b)               mu8_add(_Tp, __a, __b)
 #	define mu9_subtraction(_Tp, __a, __b)            mu8_sub(_Tp, __a, __b)
@@ -50,51 +54,59 @@ MU0_BEGIN_CDECL
 #	define mu9_division(_Tp, __a, __b)               mu8_div(_Tp, __a, __b)
 
 //#!
-//#! macro<_Tp>(_Tp & __c, _Tp & __a, _Tp & __b) : _Tp
+//#! macro{{trinary_operation}}<_Tp>(_Tp & __c, _Tp & __a, _Tp & __b) : _Tp
 //#!
 #	define mu9_multiply_accumulate(_Tp, __c, __a, __b) \
 	mu9_addition(_Tp, __c, mu9_multiplication(_Tp, __a, __b))
 
 //#!
-//#! macro<_Tp>(_Tp & __a) : _Tp
+//#! macro{{unary_operation}}<_Tp>(const _Tp & __x) : _Tp
 //#!
-#	define mu9_twice(_Tp, __a) \
-	mu9_addition(_Tp, __a, __a)
+#	define mu9_twice(_Tp, __x) \
+	mu9_addition(_Tp, __x, __x)
 
-#	define mu9_square(_Tp, __a) \
-	mu9_multiplication(_Tp, __a, __a)
+#	define mu9_square(_Tp, __x) \
+	mu9_multiplication(_Tp, __x, __x)
 
-#	define mu9_cube(_Tp, __a) \
-	mu9_multiplication(_Tp, __a, mu9_square(_Tp, __a))
+#	define mu9_cube(_Tp, __x) \
+	mu9_multiplication(_Tp, __x, mu9_square(_Tp, __x))
 
-#	define mu9_biquadrate(_Tp, __a) \
-	mu9_multiplication(_Tp, __a, mu9_cube(_Tp, __a))
+#	define mu9_biquadrate(_Tp, __x) \
+	mu9_multiplication(_Tp, __x, mu9_cube(_Tp, __x))
 
 //#!
-//#! macro<_Tp>(_Tp & __c, _Tp & __a) : _Tp
+//#! macro{{binary_operation}}<_Tp>(const _Tp & __c, const _Tp & __a) : _Tp
 //#!
 #	define mu9_square_accumulate(_Tp, __c, __a) \
 	mu9_multiply_accumulate(_Tp, __c, __a, __a)
 
 //#!
-//#! macro<_Tp>(const _Tp & __a, const _Tp & __b) : bool
+//#! macro{{binary_predicate}}<_Tp>(const _Tp & __a, const _Tp & __b) : bool
 //#!
 #	define mu9_logical_and(_Tp, __a, __b)            ( ((__a) && (__b)) ? mu0_true : mu0_false)
 #	define mu9_logical_not_and(_Tp, __a, __b)        (!((__a) && (__b)) ? mu0_true : mu0_false)
 #	define mu9_logical_or(_Tp, __a, __b)             ( ((__a) || (__b)) ? mu0_true : mu0_false)
 #	define mu9_logical_not_or(_Tp, __a, __b)         (!((__a) || (__b)) ? mu0_true : mu0_false)
-#	define mu9_logical_not(_Tp, __a)                 !(__a)
 
 //#!
-//#! macro<_Tp>(& __a, & __b) : _Tp
+//#! macro{{unary_predicate}}<_Tp>(const _Tp & __x) : bool
+//#!
+#	define mu9_logical_not(_Tp, __x)                 !(__x)
+
+//#!
+//#! macro{{binary_operation}}<_Tp>(const _Tp & __a, const _Tp & __b) : _Tp
 //#!
 #	define mu9_bit_and(_Tp, __a, __b)                ((__a) & (__b))
 #	define mu9_bit_or(_Tp, __a, __b)                 ((__a) | (__b))
 #	define mu9_bit_xor(_Tp, __a, __b)                ((__a) ^ (__b))
-#	define mu9_bit_not(_Tp, __a)                     ~(__a)
 
 //#!
-//#! @deprecated macro<_Tp>(const _Tp & __a, const _Tp & __b) : bool
+//#! macro{{unary_operation}}<_Tp>(const _Tp & __x) : _Tp
+//#!
+#	define mu9_bit_not(_Tp, __x)                     ~(__x)
+
+//#!
+//#! macro{{binary_predicate}}<_Tp>(const _Tp & __a, const _Tp & __b) : bool
 //#!
 #	define mu9_less_than(_Tp, __a, __b)              (((__a) <  (__b)) ? mu0_true : mu0_false)
 #	define mu9_less_than_or_equal(_Tp, __a, __b)     (((__a) <= (__b)) ? mu0_true : mu0_false)
@@ -104,7 +116,7 @@ MU0_BEGIN_CDECL
 #	define mu9_not_equal_to(_Tp, __a, __b)           (((__a) != (__b)) ? mu0_true : mu0_false)
 
 //#!
-//#! macro<_Tp>(const _Tp & __a, const _Tp & __b) : bool
+//#! macro{{binary_predicate}}<_Tp>(const _Tp & __a, const _Tp & __b) : bool
 //#!
 #	define mu9_less(_Tp, __a, __b)                   mu8_lt(_Tp, __a, __b)
 #	define mu9_greater(_Tp, __a, __b)                mu8_le(_Tp, __a, __b)
